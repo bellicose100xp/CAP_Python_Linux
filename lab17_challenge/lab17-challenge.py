342651a522b2bf29407bc539356e746b513d92aa
bellicose100xp <bellicose.agnostic@gmail.com>
2023-10-30 19:19:25 +0000

    Lab17 Challenge

--- lab17_challenge/lab17-challenge.py
+++ lab17_challenge/lab17-challenge.py
@@ -1,2 +1,6 @@
 def main():
-    print("test")
+    name: str = input("What is your name?: ")
+    week: str = input("What day of week it is?: ")
+    print(f"Hello, {name}! Happy {week}!")
+
+main()
