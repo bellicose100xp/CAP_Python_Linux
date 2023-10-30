1225f3bc653e0ad702829d79acb4983a65cce92c
bellicose100xp <bellicose.agnostic@gmail.com>
2023-10-30 19:50:58 +0000

    Made lab17 challenge executable

--- lab17_challenge/lab17-challenge.py
+++ lab17_challenge/lab17-challenge.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 def main():
     name: str = input("What is your name?: ")
     week: str = input("What day of week it is?: ")
     print(f"Hello, {name}! Happy {week}!")
 
-main()
+if __name__ == "__main__":
+    main()
