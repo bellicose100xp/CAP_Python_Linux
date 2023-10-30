fbe6fb08a2e5fee03ae55ff702c2597583862024
bellicose100xp <bellicose.agnostic@gmail.com>
2023-10-30 19:00:44 +0000

    added vendor name

--- lab_input/iptaker02.py
+++ lab_input/iptaker02.py
@@ -2,16 +2,20 @@
 """Alta3 Research | RZFeeser
    print() - concatenate vs print a series of items"""
 
+
 def main():
 
     # collect string input from the user
     user_input = input("Please enter an IPv4 IP address:")
-    
-    ## the line below creates a single string that is passed to print()
+
+    # the line below creates a single string that is passed to print()
     # print("You told me the IPv4 address is: " + user_input)
-    
-    ## print() can be given a series of objects separated by a comma
+
+    # print() can be given a series of objects separated by a comma
     print("You told me the IPv4 address is:", user_input)
 
-main()
+    vendor = input("Name of vendor: ")
+    print("Name of vendor is", vendor)
 
+
+main()
