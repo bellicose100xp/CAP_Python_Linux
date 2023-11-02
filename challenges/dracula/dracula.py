296602236d2961f98af74f4e305511e9979d9ee5
Chandan Chauhan <bellicose.agnostic@gmail.com>
2023-11-02 20:04:03 +0000

    dracula challenge completed

---
+++ challenges/dracula/dracula.py
@@ -0,0 +1,15 @@
+def main():
+    count = 0
+    with open(
+        "/home/student/mycode/challenges/dracula/dracula.txt", encoding="utf-8"
+    ) as f:
+        for line in f:
+            if "vampire" in line.lower():
+                print(line, end="")
+                count += 1
+
+    print(f"\nThe number of lines that contain 'vampire' are: {count}")
+
+
+if __name__ == "__main__":
+    main()
