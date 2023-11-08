3b564f0adec7717b7ff4fecd8dadedce287dccd2
Chandan Chauhan <bellicose.agnostic@gmail.com>
2023-11-08 15:55:29 +0000

    Added challenge 3 to trivia challenge

--- trivia/main.py
+++ trivia/main.py
@@ -3,15 +3,90 @@ import random
 import requests
 
 URL = "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy"
+BASE_URL = "https://opentdb.com/api.php?"
+
+CATEGORIES = {
+    9: "General Knowledge",
+    10: "Entertainment- Books",
+    11: "Entertainment- Film",
+    12: "Entertainment- Music",
+    13: "Entertainment- Musicals & Theater",
+    14: "Entertainment- Television",
+    15: "Entertainment- Video Games",
+    16: "Entertainment- Board Games",
+    17: "Science- Nature",
+    18: "Science- Computers",
+    19: "Science- Mathematics",
+    20: "Mythology",
+    21: "Sports",
+    22: "Geography",
+    23: "History",
+    24: "Politics",
+    25: "Art",
+    26: "Celebrities",
+    27: "Animals",
+    28: "Vehicles",
+    29: "Entertainment- Comics",
+    30: "Science- Gadgets",
+    31: "Entertainment- - Japanese Anime & Manga",
+    32: "Entertainment- - Cartoon Animations",
+}
+QUESTION_TYPES = ["any", "multiple", "boolean"]
+DIFFICULTIES = ["easy", "medium", "hard"]
 
 
 def main():
-    res = requests.get(URL, timeout=5)
+    print("")
+
+    # Select Category
+    for category_id, category in CATEGORIES.items():
+        print(f"{category_id}. {category}")
+
+    selected_category = input("\nPlease select a category:\n> ")
+
+    # Select Types of Question
+    for category_id, question in enumerate(QUESTION_TYPES):
+        print(f"{category_id+1}. {question}")
+
+    selected_type = input("\nPlease select a question type:\n> ")
+    api_type = QUESTION_TYPES[int(selected_type) - 1]
+
+    # Select Question Difficulty Level
+    for category_id, difficulty in enumerate(DIFFICULTIES):
+        print(f"{category_id+1}. {difficulty}")
+
+    selected_difficulty = input("\nPlease select a difficulty:\n> ")
+    api_difficulty = DIFFICULTIES[int(selected_difficulty) - 1]
+
+    # Select the number of questioins
+    selected_quantity = input("\nPlease select a quantity:\n> ")
+    api_quantity = int(selected_quantity)
+
+    # Add or remove api_type if "any" option was selected
+    final_api_type = ""
+    if api_type == "any":
+        final_api_type = ""
+    else:
+        final_api_type = f"&type={api_type}"
+
+    # Generate base URL
+    url = (
+        BASE_URL
+        + f"amount={api_quantity}&category={selected_category}&difficulty={api_difficulty}{final_api_type}"
+    )
+
+    res = requests.get(url, timeout=5)
+
     data = res.json()
     questions = data["results"]
     total_questions: int = len(questions)
-    answered_correctly: int = 0
 
+    # If no questions received, exit the program
+    if not total_questions:
+        print("No questions found. Please try again.")
+        return
+
+    answered_correctly: int = 0
     question_num: int = 1
 
     for question in questions:
@@ -24,6 +99,7 @@ def main():
         options.append(correct_answer)
         random.shuffle(options)
 
+        # List question
         print(f"\nQuestion {question_num}: {html.unescape(question['question'])}")
         for i, option in enumerate(options):
             print(f"  {i+1}. {option}")
@@ -32,6 +108,7 @@ def main():
         selected_num: int = -1
         print("Please select an option:")
 
+        # Get user input (validated)
         while not valid_selection:
             selected_option = input("> ")
 
@@ -44,6 +121,7 @@ def main():
             selected_num = int(selected_option) - 1
             valid_selection = True
 
+        # Check if answer is correct
         if options[selected_num] == correct_answer:
             print("You are correct!")
             answered_correctly += 1
@@ -52,6 +130,7 @@ def main():
 
         question_num += 1
 
+    # Display final result
     print(f"\n*** You answered {answered_correctly} of {total_questions} correctly ***")
 
 
